511268612f2ea59a5cf1b9603b9c844f2244bc1f
Bro <hitguy009@gmail.com>
2023-09-09 00:35:31 +0500

    Converted the files into typescript. Will think of what to do next

--- index.ts
+++ index.ts
@@ -1,18 +1,18 @@
 // Function for encryption
 // Default shift is 3
-function caesarCipherEncrypt(message, shift = 3) {
+function caesarCipherEncrypt(message: string, shift: number = 3): string {
   // Alright firstly, we are gonna make a string so that we can move the alphabets
-  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const alphabet: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
   // Function to shift the characters
-  function shiftCharacter(char, shift) {
+  function shiftCharacter(char:string, shift:number): string {
      if (char === " ") {
         // return spaces if there are only spaces.
         return " ";
      }
 
-     const isUpperCase = char === char.toUpperCase();
-     const charIndex = alphabet.indexOf(char.toUpperCase());
+     const isUpperCase: Boolean = char === char.toUpperCase();
+     const charIndex: number = alphabet.indexOf(char.toUpperCase());
 
      // If character is not in the alphabet string, then we don't do anything to it
      if (charIndex === -1) {
@@ -20,18 +20,18 @@ function caesarCipherEncrypt(message, shift = 3) {
      }
 
      const shiftedIndex = (charIndex + shift) % 26;
-     const shiftedChar = isUpperCase ?
+     const shiftedChar: string = isUpperCase ?
         alphabet[shiftedIndex] :
         alphabet[shiftedIndex].toLowerCase();
 
      return shiftedChar;
   }
 
-  let encryptedMessage = "";
+  let encryptedMessage: string = "";
 
   for (let i = 0; i < message.length; i++) {
-     const char = message[i];
-     const shiftedChar = shiftCharacter(char, shift);
+     const char: string = message[i];
+     const shiftedChar: string = shiftCharacter(char, shift);
      encryptedMessage += shiftedChar;
   }
 
@@ -40,24 +40,24 @@ function caesarCipherEncrypt(message, shift = 3) {
 
 // Function for Decryption
 // Default Shift value will be same as Encryption
-function caesarCipherDecrypt(encryptedMessage, shift = 3) {
+function caesarCipherDecrypt(encryptedMessage:string, shift:number = 3) : string {
   const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-  function shiftCharacter(char, shift) {
+  function shiftCharacter(char:string, shift:number) {
      if (char === " ") {
         return " ";
      }
 
-     const isUpperCase = char === char.toUpperCase();
-     const charIndex = alphabet.indexOf(char.toUpperCase());
+     const isUpperCase: boolean = char === char.toUpperCase();
+     const charIndex: number = alphabet.indexOf(char.toUpperCase());
 
      if (charIndex === -1) {
         return char;
      }
 
      // Here we use +26 so that the value is always a positive integer. cuz sometimes it may be negative.
-     const shiftedIndex = (charIndex - shift + 26) % 26;
-     const shiftedChar = isUpperCase ?
+     const shiftedIndex:number = (charIndex - shift + 26) % 26;
+     const shiftedChar:string = isUpperCase ?
         alphabet[shiftedIndex] :
         alphabet[shiftedIndex].toLowerCase();
 
@@ -67,8 +67,8 @@ function caesarCipherDecrypt(encryptedMessage, shift = 3) {
   let decryptedMessage = "";
 
   for (let i = 0; i < encryptedMessage.length; i++) {
-     const char = encryptedMessage[i];
-     const shiftedChar = shiftCharacter(char, shift);
+     const char:string = encryptedMessage[i];
+     const shiftedChar: string = shiftCharacter(char, shift);
      decryptedMessage += shiftedChar;
   }
 
